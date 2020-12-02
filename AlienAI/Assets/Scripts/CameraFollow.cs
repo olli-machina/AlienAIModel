3e3960f85fcd19666c47af1039bbb6b318968564
Olli Machina <olli.machina@mymail.champlain.edu>
2020-12-01 22:54:23 -0700

    creating new Unity project, added player and movement

---
+++ AlienAI/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    Transform playerTransform;
+    public Vector3 cameraOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerTransform.position + cameraOffset;
+    }
+}
---
+++ AlienAI/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    Vector3 newVelocity;
+
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        newVelocity = new Vector3(Input.GetAxis("Horizontal") * playerSpeed, 0.0f, Input.GetAxis("Vertical") * playerSpeed);
+        rb.velocity = newVelocity;
+    }
+}
