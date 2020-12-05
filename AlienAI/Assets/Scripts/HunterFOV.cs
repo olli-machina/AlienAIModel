05d16a3dfed9e1d562833a9233dee37c01db9b4f
Olli Machina <olli.machina@mymail.champlain.edu>
2020-12-05 12:47:46 -0700

    Hunter has search points, randomly pursues

---
+++ AlienAI/Assets/Scripts/HunterFOV.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterFOV : MonoBehaviour
+{
+    GameObject player;
+    public float fovRange = 68.0f;
+    float minDetectDistance;
+    float rayRange;
+    Vector3 facingDirection = Vector3.zero;
+    public bool pursue = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pursue = CanSeePlayer();
+        if(pursue)
+        {
+            Hunt();
+        }
+    }
+
+    public bool CanSeePlayer()
+    {
+        RaycastHit hit;
+        facingDirection = player.transform.position - transform.position;
+        float distToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if(Vector3.Angle(facingDirection, transform.forward) <= fovRange * 0.5f)
+        {
+            if (Physics.Raycast(transform.position, facingDirection, out hit, minDetectDistance))
+            {
+                return (hit.transform.CompareTag("Player"));
+            }
+        }
+     
+        return false;
+    }
+
+    public void Hunt()
+    {
+
+    }
+}
---
+++ AlienAI/Assets/Scripts/HuntingBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HuntingBehavior : MonoBehaviour
+{
+    public GameObject search1, search2, search3;
+    Vector3 destination;
+    public float speed = 15, step;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        step = speed * Time.deltaTime;
+        Patrol();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == destination)
+            Patrol();
+
+        transform.position = Vector3.MoveTowards(transform.position, destination, step);
+    }
+
+    int ChooseSearch()
+    {
+        return Random.Range(0, 3);
+    }
+
+    void Patrol()
+    {
+        int loc = ChooseSearch();
+
+        switch (loc)
+        {
+            case 0:
+                destination = search1.transform.position;
+                break;
+            case 1:
+                destination = search2.transform.position;
+                break;
+            case 2:
+                destination = search3.transform.position;
+                break;
+        }
+    }
+}
